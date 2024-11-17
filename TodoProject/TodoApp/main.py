76288bbeacc5b89061ec98b9c195a88d31f96e64
Nhan Nguyen <nhan.nguyen@27global.com>
2024-11-17 17:30:13 +0700

    Update main.py

--- TodoProject/TodoApp/main.py
+++ TodoProject/TodoApp/main.py
@@ -1,6 +1,23 @@
-from fastapi import FastAPI
+from typing import Annotated
+from sqlalchemy.orm import Session
+from fastapi import FastAPI, Depends
+from models import *
 import models
+from database import SessionLocal
 from database import engine
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+# Dependency injection really just means in programming that we need to do something before we execute
+# what we're trying to execute
+@app.get("/")
+async def read_all(db: Annotated[Session, Depends(get_db)]):
+    return db.query(Todos).all()
